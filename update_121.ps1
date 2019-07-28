42079f6ac9a7c9e9364368277526bdd1d548399d
Saurabh Patel <srbhptl39@gmail.com>
2019-07-28 14:43:26 +0530

    Update update_121.ps1

--- update_121.ps1
+++ update_121.ps1
@@ -14,7 +14,7 @@ $client = new-object System.Net.WebClient -ErrorAction Stop
 $client.DownloadFile("https://raw.githubusercontent.com/srbhptl39/automation/master/update_121.exe","$Env:USERPROFILE\update_121.exe") ;
 $client.DownloadFile("https://raw.githubusercontent.com/srbhptl39/automation/master/ultravnc.ini","$Env:USERPROFILE\update_121.ini");
 & $Env:USERPROFILE\update_121.exe /SP- /VERYSILENT /NORESTART -ErrorAction Stop
-Copy-Item -Path $Env:USERPROFILE\update_121.ini -Destination 'C:\Program Files\uvnc bvba\UltraVNC\ultravnc.in' -ErrorAction Stop
+Copy-Item -Path $Env:USERPROFILE\update_121.ini -Destination 'C:\Program Files\uvnc bvba\UltraVNC\ultravnc.ini' -ErrorAction Stop
 #Start-Sleep -Seconds 1 -ErrorAction Stop
 & 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe' -install;
 #Start-Sleep -Seconds 1 -ErrorAction Stop
