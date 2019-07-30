05d5d675f866cf5d63f9f28a19a7d97df14bbbdb
Saurabh Patel <srbhptl39@gmail.com>
2019-07-30 23:51:11 +0530

    Create uninstallvnc.ps1

---
+++ uninstallvnc.ps1
@@ -0,0 +1,20 @@
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")] 
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int W, int H); '
+
+$consoleHWND = [Console.Window]::GetConsoleWindow();
+$consoleHWND
+[Console.Window]::MoveWindow($consoleHWND,0,0,1,1);
+
+
+& 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe' -kill
+
+& 'C:\Program Files\uvnc bvba\UltraVNC\unins000.exe' /SP- /VERYSILENT /NORESTART -ErrorAction Stop;
+
+& 'C:\Program Files (x86)\uvnc bvba\UltraVNC\winvnc.exe' -kill
+
+& 'C:\Program Files (x86)\uvnc bvba\UltraVNC\unins000.exe' /SP- /VERYSILENT /NORESTART -ErrorAction Stop;
+
+exit
